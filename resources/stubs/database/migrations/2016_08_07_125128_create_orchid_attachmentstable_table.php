faa25b037a2dbdf34b49415c47cb8e10b87da30b
tabuna <bliz48rus@gmail.com>
2017-08-08 18:41:35 +0300

    migration

--- resources/stubs/database/migrations/2016_08_07_125128_create_orchid_attachmentstable_table.php
+++ resources/stubs/database/migrations/2016_08_07_125128_create_orchid_attachmentstable_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateTableAttachments extends Migration
+class CreateOrchidAttachmentstableTable extends Migration
 {
     /**
      * Run the migrations.
--- resources/stubs/database/migrations/2016_12_06_070037_create_orchid_taxonomy_table.php
+++ resources/stubs/database/migrations/2016_12_06_070037_create_orchid_taxonomy_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTaxonomyTable extends Migration
+class CreateOrchidTaxonomyTable extends Migration
 {
     /**
      * Run the migrations.
