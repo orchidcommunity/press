e0f74373368ac159073a3bc1d9ac86a2579b7c73
tabuna <bliz48rus@gmail.com>
2017-09-30 03:03:18 +0300

    - Added TinyMCE

--- src/Fields/TinyMCEField.php
+++ src/Fields/TinyMCEField.php
@@ -4,10 +4,10 @@ namespace Orchid\CMS\Fields;
 
 use Orchid\CMS\Field\Field;
 
-class SummernoteField extends Field
+class TinyMCEField extends Field
 {
     /**
      * @var string
      */
-    public $view = 'cms::fields.summernote';
+    public $view = 'cms::fields.tinymce';
 }
