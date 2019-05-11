da8f7b57e5e96ef6190ac3b4d6b747ed91da1468
PSalador <salador2005@yandex.ru>
2019-05-11 11:27:45 +0000

    add Press tests

---
+++ tests/Feature/Platform/SystemTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Feature\Platform;
+
+use Orchid\Tests\TestFeatureCase;
+
+class SystemTest extends TestFeatureCase
+{
+
+    public function testRoutePlatformSystemsMenuIndex()
+    {
+        $response = $this->actingAs($this->createAdminUser())
+            ->get(route('platform.systems.menu.index'));
+
+        $response->assertStatus(302)
+            ->assertRedirect('/dashboard/press/menu/header');
+    }
+
+    public function testRoutePlatformSystemsMenuShow()
+    {
+        $response = $this->actingAs($this->createAdminUser())
+            ->get(route('platform.systems.menu.show', 'header'));
+
+        $response->assertOk();
+        $this->assertStringContainsString('data-controller="components--menu"', $response->getContent());
+    }
+}
