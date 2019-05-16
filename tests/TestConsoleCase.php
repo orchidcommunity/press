204877594510fe98781b06027cbb6b39b67d16bd
tabuna <bliz48rus@gmail.com>
2019-05-16 23:26:52 +0300

    init phpunit

---
+++ tests/TestConsoleCase.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests;
+
+use Illuminate\Support\Str;
+use Orchestra\Testbench\TestCase;
+
+/**
+ * Class TestConsoleCase.
+ */
+abstract class TestConsoleCase extends TestCase
+{
+    use Environment;
+
+    /**
+     * @return string
+     */
+    public function generateNameFromMethod(): string
+    {
+        return studly_case(debug_backtrace()[1]['function'].Str::random());
+    }
+}
